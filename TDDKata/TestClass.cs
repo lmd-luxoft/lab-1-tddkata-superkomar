82fb99ccad3ddabfdb6d8265d62fe48ade7eead3
unknown <Sergey Komarov>
2021-05-11 13:13:12 +0300

    Add some tests methonds

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -10,11 +10,75 @@ namespace TDDKata
     public class TestClass
     {
         [Test]
-        public void SimpleTest()
+        public void SumZeroNums()
         {
             StringCalc calc = new StringCalc();
-            int value = calc.Sum("2,2");
-            Assert.That(value, Is.EqualTo(4), "Wrong actual value");
+            int value = calc.Sum("");
+            Assert.That(value, Is.EqualTo(0), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumOneNums()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("1");
+            Assert.That(value, Is.EqualTo(1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumTwoNums()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("1,2");
+            Assert.That(value, Is.EqualTo(3), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumThreeNums()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("1,2,3");
+            Assert.That(value, Is.EqualTo(6), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumFourNums()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("1,2,3,4");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SumNegativeNums()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("1,2,-3");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void LettersArguments()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("art");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SpecSymbolsArguments()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("&,:#$");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
+        }
+
+        [Test]
+        public void SpaceAsArguments()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum("   ,   ");
+            Assert.That(value, Is.EqualTo(-1), "Wrong actual value");
         }
     }
 }
